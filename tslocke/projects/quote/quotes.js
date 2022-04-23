4b8dfabdb2449b8dbc58f21a18ed4153f1519530
Tori <tori.locke@hotmail.com>
2022-04-23 15:57:55 -0700

    finished quote project

---
+++ tslocke/projects/quote/quotes.js
@@ -0,0 +1,10 @@
+const localQuotes = [
+  {
+    text: "Genius is on percent insiration and ninety-nine percent perpiration",
+    author: "Thomas Edison",
+  },
+  {
+    text: "you can observe a lot just by watching.",
+    author: "Yogi Berra",
+  },
+];
--- tslocke/projects/quote/script.js
+++ tslocke/projects/quote/script.js
@@ -1,18 +1,69 @@
+// retrieve information for index.html / user interface
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
 // Retrieve quotes from https://quotes-react.netlify.app/ api
 
 let apiQuotes = []; // set empty array to later pass in json
 
+// show loading information with loader
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// hide loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
+// wrap math random in math floor to get whole integer to pick a random quote from api
+function newQuote() {
+  loading();
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  // check if author is blank
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // adjust css if quote is long
+  if (quote.text.length > 90) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  // set quote and hide loader
+  quoteText.textContent = quote.text;
+  complete();
+}
+
 async function getQuotes() {
+  loading();
   const apiURL = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiURL);
     apiQuotes = await response.json(); // retrieve json object into variable
-    console.log(apiQuotes[11]);
+    newQuote();
   } catch (error) {
     // Error handeling
     alert(error);
   }
 }
+
+// Retweet the quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank"); //open in new tab
+}
+// Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
 // on load run the getQuotes function
 getQuotes();
